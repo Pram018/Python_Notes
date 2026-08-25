ec0ed0566bc9c19e592fdc9ecadece92d0e485e9
Pram018 <pramodhs316@gmail.com>
2026-08-25 17:35:41 +0000

    Updating the classes.py ...

--- classes.py
+++ classes.py
@@ -1,6 +1,18 @@
 class Person:
-    def talk()
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        self.score = 0
 
+    def add_score(self, points):
+        self.score += points
 
-terry = Person()
-print(terry)
+terry = Person("Pramodh", "S")
+fred = Person("Prathap", "S")
+
+print(terry.first, terry.last)
+print(fred.first, fred.last)
+
+print(fred.score)
+fred.add_score(10)
+print(fred.score)
