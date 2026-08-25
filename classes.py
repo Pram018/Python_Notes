c9d5fc4247d72fce81b015f66d39761940b16fe0
Pram018 <pramodhs316@gmail.com>
2026-08-25 17:52:25 +0000

    Updating the classes.py

--- classes.py
+++ classes.py
@@ -1,11 +1,16 @@
 class Person:
-    def __init__(self, first, last):
+    def __init__(self, first: str, last: str):
         self.first = first
         self.last = last
         self.score = 0
 
-    def add_score(self, points):
+    def add_score(self, points: int):
         self.score += points
+
+    # In __repr__ method we are expecting to always return a str
+    def __repr__(self) -> str:
+        return self.first + " " + self.last
+
 
 terry = Person("Pramodh", "S")
 fred = Person("Prathap", "S")
@@ -16,3 +21,5 @@ print(fred.first, fred.last)
 print(fred.score)
 fred.add_score(10)
 print(fred.score)
+
+print(terry)
